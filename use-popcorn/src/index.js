752ba3322ac49b4c9de3529c4b825e996bbdeab4
Andrii <phytsyk@gmail.com>
2023-10-08 19:27:09 +0200

    created star rating component

--- use-popcorn/src/index.js
+++ use-popcorn/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Stars from './Stars';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Stars maxRating={4}></Stars>
   </React.StrictMode>
 );
